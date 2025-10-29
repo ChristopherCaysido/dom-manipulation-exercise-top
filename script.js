56b71e9c475272b6952fc92188aee2cc0874a075
ChristopherCaysido <christopher.caysido@gmail.com>
2025-10-29 16:02:36 +0800

    feat: did procedure three - create a div with pink background and black border - inserted a text with "I'm in a div" text - inserted a paragraph tag with "ME TOO" text

--- script.js
+++ script.js
@@ -16,4 +16,13 @@ headingThree.style.color = 'blue';
 headingThree.textContent = "I'am a blue h3!";
 container.appendChild(headingThree);
 
-
+const pinkDiv = document.createElement("div");
+pinkDiv.style.backgroundColor = 'pink';
+pinkDiv.style.cssText = "background:pink; border: 2px black solid;"
+const headingText = document.createElement('div');
+headingText.textContent = "I'm in a div";
+const paragraphText = document.createElement('p');
+paragraphText.textContent = "ME TOO!";
+pinkDiv.appendChild(headingText);
+pinkDiv.appendChild(paragraphText);
+container.appendChild(pinkDiv);
